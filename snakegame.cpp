e95649978e6ae9b6c587123488ab603f6a11c776
pheebee14 <nphi5632@gmail.com>
2025-12-03 18:04:42 +0800

    Refactor snake game structure and initialization

--- snakegame.cpp
+++ snakegame.cpp
@@ -4,23 +4,51 @@
 #include <thread>
 #include <cstdlib>
 
-std::vector<std::vector<char>> build_board(const int& row, const int& columns);
-void print_board(const std::vector<std::vector<char>>& given_board);
-void draw_snake(std::vector<std::vector<char>>& given_board, const int& row, const int& columns)
+/*
+    Enum class for movement.
+*/
 
 struct Coordinates {
     int row;
     int column;
-}
+};
+
+struct Snake_body {
+    std::vector<Coordinates> segments;
+};
+
+std::vector<std::vector<char>> build_board(const int& row, const int& columns);
+void print_board(const std::vector<std::vector<char>>& given_board);
+void draw_snake(std::vector<std::vector<char>>& given_board, Snake_body& segments, int& rows, int& columns);
+void snake_movement();
 
 int main () {
-    int rows = 20;
-    int columns = 20;
+    int rows {20};
+    int columns {20};
+    int initial_size {3};
 
-    std::vector<std::vector<char>> board = build_board(rows, columns);
+    /*
+    SAVE FOR SESSION ON LAST HOUR: FIGURE OUT HOW TO IMPLEMENT VECTOR OF COORDINATES THROUGH STRUCT,
+    AND PASS THIS AS THE SNAKE'S BODY?
+    */
+
+    std::vector<std::vector<char>> board {build_board(rows, columns)};
+    
+    Snake_body segments;
+    segments.segments.resize(initial_size);
+
+    for (int i = 0; i < initial_size; i++){
+        segments.segments[i].row = rows / 2;
+        segments.segments[i].column = (columns / 2) - i;
+    }
 
         while(true){
-        print_board(board);
+        std::vector<std::vector<char>> frame = board;
+
+        draw_snake(frame, segments, rows, columns);
+
+        print_board(frame);
+
         std::this_thread::sleep_for(std::chrono::milliseconds(150));
         std::system("clear");
         }
@@ -56,6 +84,12 @@ void print_board(const std::vector<std::vector<char>>& given_board){
     }
 }
 
-void draw_snake(std::vector<std::vector<char>>& given_board, const int& row, const int& columns){
+void draw_snake(std::vector<std::vector<char>>& given_board, Snake_body& segments, int& rows, int& columns){
+    for (auto& member_body : segments.segments){
+        given_board[member_body.row][member_body.column] = 'o';
+    }
+}
 
+void snake_movement(){
+    // Using enum class for movement, produce switch cases for movement.
 }
